57dca11ae8607a62f7a87434e3008ae2337950c3
me <me@yogadora.local>
2025-09-16 18:27:56 -0400

    Added a blank line before defining TOKEN_FILE in config.bash.

--- config.bash
+++ config.bash
@@ -10,4 +10,5 @@ CURLPILOT_CONFIG_DIR=${CURLPILOT_CONFIG_DIR:-"$HOME/.config/curlpilot"}
 if [[ ! -d "$CURLPILOT_CONFIG_DIR" ]]; then
   mkdir -p "$CURLPILOT_CONFIG_DIR"
 fi
+
 TOKEN_FILE="$CURLPILOT_CONFIG_DIR/token.txt"
